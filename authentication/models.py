f70a21b4f1657467838eba795161f690d656ff21
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-04-02 10:46:13 -0400

    Created initial Account migration, created AccountManager

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,31 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
+
+        account = self.model(
+            email=self.normalize_email(email),
+            username=kwargs.get('username')
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
 
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
@@ -21,7 +47,7 @@ class Account(AbstractBaseUser):
 
     def __unicode__(self):
         return self.email
-        
+
     def get_full_name(self):
         return ' '.join([self.first_name, self.last_name])
 
